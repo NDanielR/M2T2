b9743b6f402578a0eff2f61e55e23064a5287411
Nelson Daniel Ramirez <n.danielramirez.h@outlook.com>
2025-07-28 16:05:39 -0500

    Add vehicle instances and method calls in main
    Replaces the Hello World output with instantiation of Car, Motorcycle, and Truck objects, and calls their mover and star methods. Demonstrates usage of the Vehicle class hierarchy.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Car vehicle1 = new Car("Toyota", "Corolla", 2022, 4);
+        Motorcycle vehicle2 = new Motorcycle("Honda", "CBR500R", 2023, 500.0);
+        Truck vehicle3 = new Truck("Volvo", "FH16", 2021, 25000.0);
+        Vehicle vehicle4 = new Car("kia","soluto",2025,4);
+
+        vehicle1.mover();
+        vehicle1.star();
+        vehicle2.mover();
+        vehicle2.star();
+        vehicle3.mover();
+        vehicle3.star();
+        vehicle4.mover();
+
     }
 }
